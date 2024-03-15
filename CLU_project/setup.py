f65f21b53f566c6e9d4dbc9aed6738b6fe95a736
Dbaba <zakaria-dbaba@hotmail.com>
2024-03-15 18:22:06 +0100

    Added .gitignore patterns for setup.py

---
+++ CLU_project/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='CLU_project',
+    version='',
+    packages=['config', 'clients', 'handlers'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Dbaba',
+    author_email='',
+    description=''
+)
